69e91312e934667d40a3f4337700b0c15c38e00a
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2020-03-26 09:53:19 -0400

    Bump version to 0.1.0 * Hashing Analyzers - These look for older or outdated hashing algorithms used in codebases

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,2 @@
-#### 0.1.0-beta006 - 2020-03-26
+#### 0.1.0 - 2020-03-26
 * Hashing Analyzers - These look for older or outdated hashing algorithms used in codebases
--- src/BinaryDefense.FSharp.Analyzers.Hashing/AssemblyInfo.fs
+++ src/BinaryDefense.FSharp.Analyzers.Hashing/AssemblyInfo.fs
@@ -8,8 +8,8 @@ open System.Reflection
 [<assembly: AssemblyMetadataAttribute("ReleaseDate","2020-03-26T00:00:00.0000000")>]
 [<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseChannel","beta")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","7242b686721e6c38013259f83e94dd9150147344")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","791957f2ad776d41ac920184b1d51151352f62ac")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -19,5 +19,5 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyMetadata_ReleaseDate = "2020-03-26T00:00:00.0000000"
     let [<Literal>] AssemblyFileVersion = "0.1.0"
     let [<Literal>] AssemblyInformationalVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseChannel = "beta"
-    let [<Literal>] AssemblyMetadata_GitHash = "7242b686721e6c38013259f83e94dd9150147344"
+    let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
+    let [<Literal>] AssemblyMetadata_GitHash = "791957f2ad776d41ac920184b1d51151352f62ac"
