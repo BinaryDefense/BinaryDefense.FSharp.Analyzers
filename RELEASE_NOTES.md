1911a3d1ae70ec1f5530a89fecbaa47dde9cf5ee
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2020-04-17 07:33:02 -0400

    Bump version to 0.2.0-beta001 * CHANGES: Update to FSharp.Analyzers.SDK 0.4.1 (https://github.com/BinaryDefense/BinaryDefense.FSharp.Analyzers/pull/4)

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+#### 0.2.0-beta001 - 2020-03-30
+* CHANGES: Update to FSharp.Analyzers.SDK 0.4.1 (https://github.com/BinaryDefense/BinaryDefense.FSharp.Analyzers/pull/4)
+
 #### 0.1.1 - 2020-03-30
 * MINOR: Using type definitions FullName instead of hard coded strings (https://github.com/BinaryDefense/BinaryDefense.FSharp.Analyzers/pull/3)
 
--- tests/examples/hashing/AssemblyInfo.fs
+++ tests/examples/hashing/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("hashing")>]
 [<assembly: AssemblyProductAttribute("BinaryDefense.FSharp.Analyzers")>]
-[<assembly: AssemblyVersionAttribute("0.1.1")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseDate","2020-03-30T00:00:00.0000000")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.1")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","083957ca042c4c1585e355386b3052e50dfa74b3")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.2.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseChannel","beta")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","dd8df95770cbed3cc9a0b30d730d66ee945b0bda")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "hashing"
     let [<Literal>] AssemblyProduct = "BinaryDefense.FSharp.Analyzers"
-    let [<Literal>] AssemblyVersion = "0.1.1"
+    let [<Literal>] AssemblyVersion = "0.2.0"
     let [<Literal>] AssemblyMetadata_ReleaseDate = "2020-03-30T00:00:00.0000000"
-    let [<Literal>] AssemblyFileVersion = "0.1.1"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.1"
-    let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "083957ca042c4c1585e355386b3052e50dfa74b3"
+    let [<Literal>] AssemblyFileVersion = "0.2.0"
+    let [<Literal>] AssemblyInformationalVersion = "0.2.0"
+    let [<Literal>] AssemblyMetadata_ReleaseChannel = "beta"
+    let [<Literal>] AssemblyMetadata_GitHash = "dd8df95770cbed3cc9a0b30d730d66ee945b0bda"
