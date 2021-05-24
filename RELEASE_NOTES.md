0e29dad86df6c90b351d8c7af9d15c2643143f1f
Jimmy Byrd <jimmybyrd@gmail.com>
2021-05-23 20:07:25 -0400

    Bump version to 0.2.2-beta001 * CHANGES: [update to Analyzer SDK 0.8.0, FSharp.Core 5.0.1, FCS 39.0.0](https://github.com/BinaryDefense/BinaryDefense.FSharp.Analyzers/pull/7)

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,10 +1,19 @@
+#### 0.2.2-beta001 - 2021-05-23
+
+* CHANGES: [update to Analyzer SDK 0.8.0, FSharp.Core 5.0.1, FCS 39.0.0](https://github.com/BinaryDefense/BinaryDefense.FSharp.Analyzers/pull/7)
+
 #### 0.2.1 - 2021-01-21
-* CHANGES: Update to FSharp.Analyzers.SDK 0.7.0 (https://github.com/BinaryDefense/BinaryDefense.FSharp.Analyzers/pull/6)
+
+* CHANGES: [Update to FSharp.Analyzers.SDK 0.7.0](https://github.com/BinaryDefense/BinaryDefense.FSharp.Analyzers/pull/6)
+
 #### 0.2.0-beta002 - 2020-03-30
-* CHANGES: Update to FSharp.Analyzers.SDK 0.4.1 (https://github.com/BinaryDefense/BinaryDefense.FSharp.Analyzers/pull/4)
+
+* CHANGES: [Update to FSharp.Analyzers.SDK 0.4.1](https://github.com/BinaryDefense/BinaryDefense.FSharp.Analyzers/pull/4)
 
 #### 0.1.1 - 2020-03-30
-* MINOR: Using type definitions FullName instead of hard coded strings (https://github.com/BinaryDefense/BinaryDefense.FSharp.Analyzers/pull/3)
+
+* MINOR: [Using type definitions FullName instead of hard coded strings](<ttps://github.com/BinaryDefense/BinaryDefense.FSharp.Analyzers/pull/3)
 
 #### 0.1.0 - 2020-03-26
+
 * Hashing Analyzers - These look for older or outdated hashing algorithms used in codebases
--- tests/examples/hashing/AssemblyInfo.fs
+++ tests/examples/hashing/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("hashing")>]
 [<assembly: AssemblyProductAttribute("BinaryDefense.FSharp.Analyzers")>]
-[<assembly: AssemblyVersionAttribute("0.2.1")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-01-21T00:00:00.0000000")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.2.1")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","85c69689a2c555beda673c375d6d97032c192df9")>]
+[<assembly: AssemblyVersionAttribute("0.2.2")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-05-23T00:00:00.0000000")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.2")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.2.2")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseChannel","beta")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","9409e7015fdd7fced83a3d7b283a731c006e70c9")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "hashing"
     let [<Literal>] AssemblyProduct = "BinaryDefense.FSharp.Analyzers"
-    let [<Literal>] AssemblyVersion = "0.2.1"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-01-21T00:00:00.0000000"
-    let [<Literal>] AssemblyFileVersion = "0.2.1"
-    let [<Literal>] AssemblyInformationalVersion = "0.2.1"
-    let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "85c69689a2c555beda673c375d6d97032c192df9"
+    let [<Literal>] AssemblyVersion = "0.2.2"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-05-23T00:00:00.0000000"
+    let [<Literal>] AssemblyFileVersion = "0.2.2"
+    let [<Literal>] AssemblyInformationalVersion = "0.2.2"
+    let [<Literal>] AssemblyMetadata_ReleaseChannel = "beta"
+    let [<Literal>] AssemblyMetadata_GitHash = "9409e7015fdd7fced83a3d7b283a731c006e70c9"
